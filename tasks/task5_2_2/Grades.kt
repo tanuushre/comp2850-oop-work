e464ddf9ffdd76f74dd9951a000fae0480ed5232
tanuushre <tanuushre.uk@gmail.com>
2025-10-09 15:32:51 +0000

    up to 8. Lambda Expressions

--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,1 +1,18 @@
 // Task 5.2.2: conversion of marks into grades, using a function
+fun grade(mark: Int) = when (mark) {
+    in 0..39    -> "Fail"
+    in 40..69   -> "Pass"
+    in 70..100  -> "Distinction"
+    else        -> "?"
+} 
+
+fun main(args: Array<String>){
+    for (arg in args) {
+        val mark = arg.toIntOrNull()
+        if (mark != null) {
+            println("$mark is a ${grade(mark)}")
+        } else {
+            println("'$arg' is not a valid number.")
+        }
+    }
+}
